fb82a0a622553878bd0de1215e6ffbda0d93f1c1
b1mind <brent@1mindsolutions.com>
2020-12-26 19:02:06 -0700

    🧦 playing with some gsap

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,54 +1,133 @@
 <script>
+  import { gsap } from 'gsap'
   import { animate } from '../anime.js'
 
   export let thumb
   export let title
   export let date
+  export let author
+  export let ups
+  let isToggled
+  const tl = gsap.timeline({})
+
+  function cardAnimate(e) {
+    const height = e.target.height
+    const thumbnail = e.target.closest('.thumbnail')
+    const caption = thumbnail.nextElementSibling
+
+    if (isToggled) {
+      tl.reverse()
+      isToggled = false
+      return
+    }
+
+    isToggled = true
+
+    tl.add('start')
+      .set('.overlay', { display: 'block' })
+      .set(thumbnail.parentElement, { zIndex: 999 })
+      .to(caption.children, { duration: 0.5, x: -400 }, 'start')
+      .to(
+        thumbnail,
+        {
+          duration: 0.5,
+          position: 'absolute',
+          duration: 1,
+          height: height,
+          zIndex: 999,
+        },
+        '-=0.5 ',
+      )
+  }
 </script>
 
-<figure class="card" use:animate={{ type: 'from', duration: 1, opacity: 0, x: -100 }}>
-  <div class="img"><img src={thumb} alt={title} /></div>
-  <figcaption>{title} <i>- {date}</i></figcaption>
+<div class="overlay" />
+
+<figure class="card" use:animate={{ type: 'from', duration: 1, autoAlpha: 0, y: 100 }}>
+  <div class="thumbnail" on:click={cardAnimate}>
+    <img src={thumb} alt={title} loading="lazy" />
+  </div>
+
+  <figcaption data-ups={ups}>
+    <i>{author} {date}</i>
+    <h2>{title}</h2>
+  </figcaption>
 </figure>
 
 <style type="text/scss">
   //< style like its 1989
 
-  figure {
-    display: grid;
-    place-items: center;
-    grid-template-columns: 2fr, 1fr;
-    grid-template-rows: 1fr;
-    background-color: var(--clr-two);
-    border-radius: 0.25rem;
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: none;
+    background: rgba(0, 0, 0, 0.2);
+    opacity: 0.5;
+    z-index: 600;
   }
 
-  .img {
-    width: 150px;
-    max-height: 140px;
+  figure {
+    position: relative;
+    min-height: 250px;
+    margin: 0;
+    display: grid;
+    background-color: var(--clr-white);
+    border-radius: 0.25rem;
+    z-index: 0;
+  }
 
-    margin: 1rem;
-    padding: 0.5rem;
-    text-align: center;
+  .thumbnail {
+    height: 200px;
+    display: grid;
     background-color: #fff;
     border-radius: 0.25rem;
     overflow: hidden;
+    cursor: pointer;
 
     img {
-      max-width: 100%;
+      width: 100%;
+      min-width: 360px;
       object-fit: cover;
       border-radius: 0.25rem;
     }
   }
 
   figcaption {
+    position: absolute;
+    bottom: 0;
     width: 100%;
-    padding: 1rem;
     color: var(--clr);
-    background-color: var(--clr-two-dark);
+    overflow: hidden;
+
+    &::after {
+      content: attr(data-ups) '';
+      position: absolute;
+      right: 0;
+      bottom: 20px;
+      width: 3.5rem;
+      height: 3.5rem;
+      display: grid;
+      place-items: center;
+      background-color: var(--clr-two);
+      border-radius: 50%;
+      z-index: 999;
+    }
+
     i {
+      width: max-content;
+      padding: 0.25rem 0.75rem;
       display: block;
       font-size: 0.75rem;
+      background-color: var(--clr-two);
+      border-radius: 0 1rem 0 0;
+    }
+
+    h2 {
+      margin: 0;
+      padding: 0.5rem;
+      font-size: 1rem;
+      background-color: var(--clr-two-dark);
     }
   }
 
